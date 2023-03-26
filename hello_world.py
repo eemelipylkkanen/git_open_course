11f0b2fbdf9afae4308d1ebdac6b213c242838cc
Eemeli <eemeli1234@gmail.com>
2023-03-26 17:40:03 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,4 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello once again")
-
-=======
 print("Hello darkness my old friend")
->>>>>>> inter_3/master
-=======
 print("I've come to talk with you again")
->>>>>>> inter_3/feature/modify-print
+
